7ceb81739d1bc7b2453a0b9fa0a3f5aeec04ad20
lethze <thebiwon@gmail.com>
2024-04-06 00:01:40 +0900

    video player #1

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,51 @@
-console.log("video player!");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const hadlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handlePause = () => {
+  playBtn.innerText = "Play";
+};
+
+const handlePlay = () => {
+  playBtn.innerText = "Pause";
+};
+
+const hadleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+playBtn.addEventListener("click", hadlePlayClick);
+muteBtn.addEventListener("click", hadleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
