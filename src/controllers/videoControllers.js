53680757ece455c0bc0f379315176ae1c65115df
lethze <thebiwon@gmail.com>
2024-04-19 01:09:03 +0900

    add comment

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 import User from "../models/User";
 
 export const home = async (req, res) => {
@@ -8,8 +9,9 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
-  const id = req.params.id;
-  const video = await Video.findById(id).populate("owner");
+  const { id } = req.params;
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   } else {
@@ -124,4 +126,24 @@ export const registerView = async (req, res) => {
   video.meta.views++;
   await video.save();
   return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { text },
+    session: { user },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  video.save();
+  return res.sendStatus(201); //Created
 };
